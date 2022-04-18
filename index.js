e0fcb2616dca59d5a51e14e3c459ced4b45f79e2
Vigilante2006 <82402212+Vigilante2006@users.noreply.github.com>
2022-04-19 00:41:15 +0530

    responsive commit

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ function register() {
     x.style.left = '-400px';
     y.style.left = '50px';
     z.style.left = '110px';
-    
+
 }
 function login() {
     x.style.left = '50px';
     y.style.left = '450px';
     z.style.left = '0px';
 }
+
+
+// // for nav Transition on mobile
+
+// let hamp = document.getElementsByClassName('hamp');
+// let miniNav = document.getElementById('miniNav');
+
+// hamp.addEventListener('click', event => {
+//     if (event.target.checked)  {
+//         miniNav.style.opacity = "1";
+//         alert("checked")
+//     }
+//     else {
+//         miniNav.style.opacity = "0";
+//     }
+// });
